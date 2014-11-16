372fced9f69e210dc7c3ace9a0e77138fb941f0c
McFly Weng <McFly@McFlycn-TechReview-2.local>
2014-11-15 23:15:46 -0500

    ffff

---
+++ hackfoo/CLICHE.h
@@ -0,0 +1,13 @@
+//
+//  CLICHE.h
+//  hackfoo
+//
+//  Created by McFly Weng on 14/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CLICHE : NSObject
+
+@end
---
+++ hackfoo/CLICHE.m
@@ -0,0 +1,13 @@
+//
+//  CLICHE.m
+//  hackfoo
+//
+//  Created by McFly Weng on 14/11/15.
+//
+//
+
+#import "CLICHE.h"
+
+@implementation CLICHE
+
+@end
